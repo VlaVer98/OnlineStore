452839f7faa7c09aefdc95aa6e047c9115f11d82
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-26 15:09:05 +0700

    Added Entities New entities: -RoleClaim -UserRole -UserToken

---
+++ Shop.Domain/Models/Identity/RoleClaim.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Shop.Domain.Models.Identity
+{
+    public class RoleClaim : IdentityRoleClaim<Guid>
+    {
+    }
+}
---
+++ Shop.Domain/Models/Identity/UserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Shop.Domain.Models.Identity
+{
+    public class UserRole : IdentityUserRole<Guid>
+    {
+    }
+}
