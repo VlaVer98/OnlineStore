348b4c20b8f9e2fa6cb979d59ae2d8b6b9e6ab95
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-28 13:07:08 +0700

    Added to getting all categories in controller -Created CategoryController and action GetAll. -Added view index for controller

---
+++ Shop.WEB/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Contracts.Services;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using Shop.Domain.Models.Dtos.Category;
+using AutoMapper;
+using Shop.WEB.Models.ViewModels;
+
+namespace Shop.WEB.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public CategoryController(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<CategoryDto> categoriesDTO = _serviceProvider.GetService<ICategoryService>().GetAll();
+            IEnumerable<CategoryViewModel> categoriesVM = _serviceProvider.GetService<IMapper>()
+                .Map<IEnumerable<CategoryViewModel>>(categoriesDTO);
+
+            return View(categoriesVM);
+        }
+    }
+}
