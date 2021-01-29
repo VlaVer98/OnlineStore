e41ceb3047d336713da497cc3a8bee222faf07c7
VlaVer98 <verholantsev1998@yandex.ru>
2021-01-29 18:57:58 +0700

    Added ImageService

---
+++ Shop.Logic.BLL/Services/ImageService.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Shop.Domain;
+using Shop.Domain.Contracts.Services;
+using Shop.Logic.BLL.Services.Base;
+
+namespace Shop.Logic.BLL.Services
+{
+    public class ImageService : BaseService, IImageService
+    {
+        public ImageService(IUnitOfWork unitOfWork, IMapper mapper)
+            : base (unitOfWork, mapper) { }
+    }
+}
