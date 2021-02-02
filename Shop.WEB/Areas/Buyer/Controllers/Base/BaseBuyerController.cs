47742372a0641407f3d55c9f497c417989a2169d
VlaVer98 <verholantsev1998@yandex.ru>
2021-02-02 17:27:09 +0700

    Created a personal area for buyer

---
+++ Shop.WEB/Areas/Buyer/Controllers/Base/BaseBuyerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.WEB.Areas.Buyer.Controllers.Base
+{
+    [Area("Buyer")]
+    public class BaseBuyerController : Controller
+    {
+        private readonly IServiceProvider _services;
+
+        public BaseBuyerController(IServiceProvider service)
+        {
+            _services = service;
+        }
+    }
+}
---
+++ Shop.WEB/Areas/Buyer/Controllers/ProfileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.WEB.Areas.Buyer.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.WEB.Areas.Buyer.Controllers
+{
+    public class ProfileController : BaseBuyerController
+    {
+        public ProfileController(IServiceProvider service)
+            : base(service) { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
