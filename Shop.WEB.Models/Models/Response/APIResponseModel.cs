7a1278a6a56e5e8093a31603d9bf22741ee6f45e
VlaVer98 <verholantsev1998@yandex.ru>
2021-02-08 13:39:52 +0700

    Created response model for api controller

---
+++ Shop.WEB.Models/Models/Response/APIResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.WEB.Models.Models.Response
+{
+    class APIResponseModel<T> where T : class
+    {
+        public bool IsSuccessful { get; set; }
+        public List<string> Messages { get; set; }
+        public T Data { get; set; }
+    }
+}
