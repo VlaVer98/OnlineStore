1cd1ff70bf329da910b6d279129391c2266f95a2
VlaVer98 <verholantsev1998@yandex.ru>
2021-02-10 16:29:28 +0700

    Adding basic user interface to identity server

--- Shop.WEB.IdentityServer/Startup.cs
+++ Shop.WEB.IdentityServer/Startup.cs
@@ -16,6 +16,8 @@ namespace Shop.WEB.IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
             var builder = services.AddIdentityServer()
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients);
@@ -31,17 +33,16 @@ namespace Shop.WEB.IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseIdentityServer();
+            //app.UseAuthorization();
 
-/*            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });*/
+                endpoints.MapDefaultControllerRoute();
+            });
         }
     }
 }
